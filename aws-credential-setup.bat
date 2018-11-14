473fb27afd30785170e5268dea49bdc43c331ea9
Raja Khan <rkhan@raileurope.com>
2018-11-14 03:28:47 -0500

    Terraform script to single web server

---
+++ aws-credential-setup.bat
@@ -0,0 +1,10 @@
+#Linux, macOS, or Unix
+
+#$ export AWS_ACCESS_KEY_ID=
+#$ export AWS_SECRET_ACCESS_KEY=
+#$ export AWS_DEFAULT_REGION=us-west-2
+
+
+#Windows
+set AWS_ACCESS_KEY_ID=
+set AWS_SECRET_ACCESS_KEY=
