bbcd4ddf4e86327c5e4dfb0db19288ffd62b2cf7
Raja Khan <rkhan@MacBook-Pro-4.home>
2019-05-10 00:11:26 -0400

    Added keyapir, instance provisining data and elastic-ip association

--- main.tf
+++ main.tf
@@ -19,10 +19,13 @@ resource "aws_instance" "example" {
   # Ubuntu Server 14.04 LTS (HVM), SSD Volume Type in us-east-1
   ami = "ami-2d39803a"
   instance_type = "t2.micro"
+  key_name = "rk-ec2-keypair-1"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
+              sudo apt-get update
+              sudo apt-get -y install docker.io
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
@@ -46,4 +49,35 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  //SSH Client
+  ingress {
+      from_port = "22"
+      to_port = "22"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+
+      # Allow all inbound
+        ingress {
+          from_port   = 0
+          to_port     = 65535
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        egress {
+          from_port   = 0
+          to_port     = 65535
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
 }
